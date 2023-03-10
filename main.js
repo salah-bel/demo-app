be75d0912cce0ec35c8456122daaa33223bae44c
salah-bel <54447309+salah-bel@users.noreply.github.com>
2023-03-10 11:57:09 +0100

    products list addedd to dom

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var productList = document.getElementById("product-list");
 	image
 	titre
 	description
-	prix
+	price
 
 */
 
@@ -14,76 +14,73 @@ var products = [{
         title: "iPhone X",
         description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
         imageUrl: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-        prix: (Math.random() * 100)
+        price: (Math.random() * 100)
     },
     {
         id: 2,
-        title: "iPhone X",
+        title: "iPhone Y",
         description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
 
         imageUrl: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-        prix: (Math.random() * 100)
+        price: (Math.random() * 100)
     },
     {
         id: 3,
         title: "Samsung Universe 9",
-        description: "Samsung's new variant which goes beyond Galaxy to the Universe",
-        prix: (Math.random() * 100),
+        description: "Samsung's new variant which goes beyond Galaxy to the Universe un texte en plus ",
+        price: (Math.random() * 100),
         imageUrl: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
     },
     {
         id: 4,
         title: "OPPOF19  9",
         description: "OPPO F19 is officially announced on April 2021.Universe",
-        prix: (Math.random() * 100),
+        price: (Math.random() * 100),
         imageUrl: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
     },
     {
         id: 5,
         title: "Huawei P30",
         description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany ",
-        prix: (Math.random() * 100),
+        price: (Math.random() * 100),
         imageUrl: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
     },
     {
         id: 6,
         title: "MacBook Pro",
         description: "acBook Pro 2021 with mini-LED display may launch between September, November ",
-        prix: (Math.random() * 100),
-        imageUrl: "https://i.dummyjson.com/data/products/6/thumbnail.jpg",
+        price: (Math.random() * 100),
+        imageUrl: "https://i.dummyjson.com/data/products/78/thumbnail.jpg",
     },
     {
         id: 7,
         title: "Samsung Galaxy Book",
         description: "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
-        prix: (Math.random() * 100),
+        price: (Math.random() * 100),
         imageUrl: "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
     }, {
         id: 8,
         title: "Microsoft Surface Laptop 4",
         description: "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant ",
-        prix: (Math.random() * 100),
+        price: (Math.random() * 100),
         imageUrl: "https://i.dummyjson.com/data/products/8/thumbnail.jpg",
     }
 
 ]
 
-console.log(products)
-
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < products.length; i++) {
 
     productList.innerHTML += `
 	<div class="card" style="width: 18rem;">
-	<img src="https://media.carrefour.fr/medias/c45805f1a820359595b6ab8b424a4d1d/p_540x540/08718951340350-a1r1-s01.jpg" class="card-img-top" alt="...">
+	<img src= ${products[i].imageUrl} class="card-img-top" alt="..." height=150>
 	<div class="card-body">
-		<h5 class="card-title">Card title</h5>
-		<p class="card-text"> text to build on the card title and make up the bulk of the card's content.
-		</p>
+		<h5 class="card-title">${products[i].title}</h5>
+		<p class="card-text">${products[i].description}</p>
 		<div class="row justify-content-between">
-		<span>Prix : <strong>22</strong> &euro;</span>
+		<span>prix : <strong>${products[i].price.toFixed(2)} &euro;</strong></span> </span>
 		<a href="#" class="btn btn-primary">Plus..</a>
 		</div>
-		
+
 	</div>
 </div>
 	`
