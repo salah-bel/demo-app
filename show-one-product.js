2d6477fa53384af9c694861b3d1cfefe0554bf7f
salah-bel <54447309+salah-bel@users.noreply.github.com>
2023-03-13 15:29:00 +0100

    show one product

--- show-one-product.js
+++ show-one-product.js
@@ -1,9 +1,12 @@
+var productDetail = document.getElementById('container');
+
+
 var products = [{
         id: 1,
         title: "iPhone X",
         description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
         imageUrl: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-        price: (Math.random() * 100)
+        price: 900
     },
     {
         id: 2,
@@ -11,47 +14,47 @@ var products = [{
         description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
 
         imageUrl: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-        price: (Math.random() * 100)
+        price: 300
     },
     {
         id: 3,
         title: "Samsung Universe 9",
         description: "Samsung's new variant which goes beyond Galaxy to the Universe un texte en plus ",
-        price: (Math.random() * 100),
+        price: 1200,
         imageUrl: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
     },
     {
         id: 4,
         title: "OPPOF19  9",
         description: "OPPO F19 is officially announced on April 2021.Universe",
-        price: (Math.random() * 100),
+        price: 800,
         imageUrl: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
     },
     {
         id: 5,
         title: "Huawei P30",
         description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany ",
-        price: (Math.random() * 100),
+        price: 596,
         imageUrl: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
     },
     {
         id: 6,
         title: "MacBook Pro",
         description: "acBook Pro 2021 with mini-LED display may launch between September, November ",
-        price: (Math.random() * 100),
+        price: 201,
         imageUrl: "https://i.dummyjson.com/data/products/78/thumbnail.jpg",
     },
     {
         id: 7,
         title: "Samsung Galaxy Book",
         description: "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
-        price: (Math.random() * 100),
+        price: 999,
         imageUrl: "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
     }, {
         id: 8,
         title: "Microsoft Surface Laptop 4",
         description: "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant ",
-        price: (Math.random() * 100),
+        price: 459,
         imageUrl: "https://i.dummyjson.com/data/products/8/thumbnail.jpg",
     }
 
@@ -61,16 +64,33 @@ var params = window.location.search
 const searchParams = new URLSearchParams(params);
 var productId = searchParams.get("id");
 
-console.log(productId)
-
-
-
 // AFFICHER LE DETAIL D UN PRODUIT
 
 products.forEach(function(product) {
     if (product.id == productId) {
-        console.log(product.title)
-        console.log(product.description)
-        console.log(product.imageUrl)
+        productDetail.innerHTML = `
+		
+			<h2> ${product.title} </h2>
+			
+	        <h2  class="text-${ (product.price < 600)?"success":"" } class2">
+				 ${product.price} €
+			</h2>
+			
+	        <img src="${product.imageUrl}" alt=""> 
+		`
+            // title.textContent = product.title;
+
+        // if (product.price < 600) {
+        //     priceElm.style.color = "green"
+        // };
+
+        // priceElm.textContent = product.price + " euro";
+        // img.src = product.imageUrl
     }
 })
+
+
+
+var age = 10;
+
+console.log((age >= 18) ? "majeur" : "mineur")
