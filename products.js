297b11d35cc501dc90941a7be2d43e5abf69765b
salah-bel <54447309+salah-bel@users.noreply.github.com>
2023-03-13 15:33:49 +0100

    add products array

---
+++ products.js
@@ -0,0 +1,58 @@
+var products = [{
+        id: 1,
+        title: "iPhone X",
+        description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+        imageUrl: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        price: 900
+    },
+    {
+        id: 2,
+        title: "iPhone Y",
+        description: "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+
+        imageUrl: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+        price: 300
+    },
+    {
+        id: 3,
+        title: "Samsung Universe 9",
+        description: "Samsung's new variant which goes beyond Galaxy to the Universe un texte en plus ",
+        price: 1200,
+        imageUrl: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+    },
+    {
+        id: 4,
+        title: "OPPOF19  9",
+        description: "OPPO F19 is officially announced on April 2021.Universe",
+        price: 800,
+        imageUrl: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+    },
+    {
+        id: 5,
+        title: "Huawei P30",
+        description: "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany ",
+        price: 596,
+        imageUrl: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+    },
+    {
+        id: 6,
+        title: "MacBook Pro",
+        description: "acBook Pro 2021 with mini-LED display may launch between September, November ",
+        price: 201,
+        imageUrl: "https://i.dummyjson.com/data/products/78/thumbnail.jpg",
+    },
+    {
+        id: 7,
+        title: "Samsung Galaxy Book",
+        description: "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+        price: 999,
+        imageUrl: "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+    }, {
+        id: 8,
+        title: "Microsoft Surface Laptop 4",
+        description: "Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant ",
+        price: 459,
+        imageUrl: "https://i.dummyjson.com/data/products/8/thumbnail.jpg",
+    }
+
+]
